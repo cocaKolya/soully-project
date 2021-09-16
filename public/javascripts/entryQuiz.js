49d532eb540f035e062cb2f4f0db7fd6471a8668
Nikolay <nelidovn@gmail.com>
2021-09-16 10:00:10 +0300

    alfa version

---
+++ public/javascripts/entryQuiz.js
@@ -0,0 +1,23 @@
+const $birthData = document.forms.birthData;
+const $button = document.querySelector('.birthdata-btn');
+const $quizWrapper = document.querySelector('.quiz-wrapper');
+
+console.log(localStorage.getItem('birthData'));
+//проверка наличия в сторадже данных из первого опросника про рождение
+if (!localStorage.getItem('birthData')) {
+  $button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const myData = Object.fromEntries(new FormData($birthData));
+    let { name, date, time, place } = myData;
+    if ((name, date, time, place)) {
+      const jsonData = JSON.stringify(myData);
+      localStorage.setItem('birthData', jsonData);
+      // console.log(localStorage);
+      // const a = localStorage.getItem('birthData');
+      // console.log(a);
+      $quizWrapper.innerHTML = 
+    }
+  });
+} else if (!localStorage.getItem('questionsData')){
+
+}
---
+++ routes/user.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const authUser = require('../middlewares/authUser');
+
+router.get('/login', authUser, async (req, res) => {
+  res.render('./login');
+});
+
+router.get('/registration', authUser, async (req, res) => {
+  res.render('./registration');
+});
+
+router.post('/registration', async (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
+  if (name && email && password) {
+    const hashPass = await bcrypt.hash(password, 10);
+    try {
+      const newUser = await User.create({ name, email, password: hashPass });
+      req.session.user = {
+        id: newUser.id,
+        name: newUser.name,
+      };
+      console.log(req.session.user);
+      return res.redirect('/home');
+    } catch (err) {
+      console.log('333333333');
+      return res.redirect('/user/registration');
+    }
+  } else {
+    console.log('4444444444444');
+    return res.redirect('/user/registration');
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  if (email && password) {
+    try {
+      const currentUser = await User.findOne({ where: { email: email } });
+      if (
+        currentUser &&
+        (await bcrypt.compare(password, currentUser.password))
+      ) {
+        req.session.user = {
+          id: currentUser.id,
+          name: currentUser.name,
+        };
+        return res.redirect('/home');
+      } else {
+        return res.redirect('/user/login');
+      }
+    } catch (err) {
+      return res.redirect('/user/login');
+    }
+  } else {
+    return res.redirect('/user/login');
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.clearCookie('sId').redirect('/');
+});
+
+module.exports = router;
